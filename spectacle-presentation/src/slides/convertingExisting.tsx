86db15902a1ea1a0caf7500b0e74e13f914ba64a
Justin Cronin <jcronin@clearedgeit.com>
2022-02-21 19:24:15 +0000

    Last of the script

--- spectacle-presentation/src/slides/convertingExisting.tsx
+++ spectacle-presentation/src/slides/convertingExisting.tsx
@@ -5,10 +5,37 @@ import { SectionSlide } from '../template/baseSlide';
 export const ConvertingExisting: React.FunctionComponent = () => {
   return <SectionSlide sectionName={'Converting an Existing React App'} subHeading={'Adopting Gradually'}>
     <Notes>
-      However, what happens if you have an existing application.  
+      However, what happens if you have an existing application? Full disclosure, I'm going to be presenting this
+      as if the typescript transpiler is already being used.  If you are not using that already there will be some additional
+      work to do.  That is going to depend on your bundling tool.  No matter which you are using it is normally pretty painless.
+      If you created a react app without typescript there are instructions on how to switch that.  I'll add them at the end of the slide
+      deck.  
+      So we have a simple ToDo app here.  
+      **Show working example**
+      This is how it should look.  Its a field that allows you to type a name and create a todo.  We can then
+      complete that using the button included in the card.
+
+      **Show not working example**
+      Here is the same app.  It has not been converted yet, but we can see it renders, but it isn't working.  We don't even
+      get any console information about what is happening either.  All we know is we aren't getting any todos.  Lets jump into 
+      the code and try seeing if we can solve this without debugging and just using typescript.
+      Let's give a quick overview.  App is our application root.  It sets some state and calls child components.  We can
+      see it is in charge of maintaining the todo state.  It needs to hand down some functions to children to maintain its state.
+      We have a TodoInput which is a simple form that has a text box with a button that adds a todo to state using the given function.
+      Back up, we have our TodoCardGrid, this renders all of our todos.  We can see that it takes the given todos, breaks them up
+      into groups using given props, or defaults to 3.  We then render something called Todo for each todo.
+      Todo is a card that renders the todo.  Lastly, is CompleteButton.  This is a button that will mark the todo as complete.
+      Now that we've had the tour, lets start by converting these to typescript files.  We can do that by changing the file extensions from
+      jsx to tsx.
+      We can see right away that a fair amount of our files have red squiglies in them.  Lets start at the bottom and work our way up.
+      completeButton -> todo -> todoCardGrid -> todoInput -> App -> todoCardGrid
+
       1. Using the wrong prop for completeTodo for TodoCardGrid in App.jsx.              
       2. App.jsx sends the wrong function for addTodo                                    
-      3. App.jsx compares name todo.name to todoName, not todo.title                                  
+      3. App.jsx compares name todo.name to todoName, not todo.title      
+      4. TodoCardGrid returns an array of elements, not a single                
+
+      After we can see that the app is working as expected!  No run time debugging needed! The compiler did all of the work for us.           
     </Notes>
   </SectionSlide>;
 }
--- spectacle-presentation/src/slides/furtherResources.tsx
+++ spectacle-presentation/src/slides/furtherResources.tsx
@@ -12,6 +12,7 @@ export const FurtherResources: React.FunctionComponent = () => {
           <ListItem>TypeScript Playground - Play with TypeScript in the browser - <Link fontSize={'list'} target={'_blank'} href={'https://bit.ly/3s52IbZ'}>https://bit.ly/3s52IbZ</Link></ListItem>
           <ListItem>TypeScript Deep Dive - GitBook written by Basarat Ali Syed - <Link fontSize={'list'} target={'_blank'} href={'https://bit.ly/35f0Iox'}>https://bit.ly/35f0Iox</Link></ListItem>
           <ListItem>React Tutorial - Tic Tac Toe Game in React - <Link fontSize={'list'} target={'_blank'} href={'https://bit.ly/36u2H95'}>https://bit.ly/36u2H95</Link></ListItem>
+          <ListItem>Add TypeScript to create-react-app - <Link fontSize={'list'} target={'_blank'} href={'https://bit.ly/3v6nklK'}>https://bit.ly/3v6nklK</Link></ListItem>
         </UnorderedList>
       </Box>
       <Box>
@@ -23,7 +24,8 @@ export const FurtherResources: React.FunctionComponent = () => {
       That brings us to the end of the presentation.  I did want to call out some additional resources for you to explore if you wanted to find out more. 
       The TypeScript playground is out there.  It's a REPL app that lets you write TypeScript and it will show you what it transpiles to in the next pane.  Its a neat way to try stuff out.
       Next is the TypeScript Deep Dive Book.  This book is still being updated and its a really strong place for best practices.  Its free online through GitBook
-      Lastly, if you haven't written a react app before, you can use some of the tools from this talk and follow the instructions on the Tic Tac Toe tutorial on the React Website.
+      If you haven't written a react app before, you can use some of the tools from this talk and follow the instructions on the Tic Tac Toe tutorial on the React Website.
+      As mentioned before, I've added a link here to instructions on how to convert your react app that used create-react-app to typescript as well.
 
       One last thing I wanted to call out. In the interest of eating my own dogfood.  This presentation was built using spectacle.  Its a library built on react to make presentation.  So this 
       entire slide deck is actually built in TypeScript and React.  If you want to find out more about the presentation tool you can go to that link to explore spectacle.
